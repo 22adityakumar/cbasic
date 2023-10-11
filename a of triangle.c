a7a2297323ec9ca1c90891003544e6f44820646e
Aditya Kumar <147469835+22adityakumar@users.noreply.github.com>
2023-10-11 12:06:28 +0530

    Add files via upload

---
+++ a of triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int area,b,h;
+	printf("enter the value of b");
+	scanf("%d",&b);
+	printf("enter the value of h");
+	scanf("%d",&h);
+	area=0.5*b*h;
+	printf("%d",area);
+}
